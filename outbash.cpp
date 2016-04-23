dd7fd9a1e2a02f927eaa94ad7c07ed25ffc1957e
Guillaume Knispel <xilun0@gmail.com>
2016-04-24 01:38:42 +0200

    fix Win32_perror fallback message

--- outbash.cpp
+++ outbash.cpp
@@ -94,7 +94,7 @@ static void Win32_perror(const char* what)
                                     nullptr, (DWORD)errnum, 0, (LPWSTR)&str,
                                     0, nullptr);
     if (nbWChars == 0) {
-        fprintf(stderr, "%s%ssocket error %d (FormatMessage failed)\n",
+        fprintf(stderr, "%s%swin32 error %d (FormatMessage failed)\n",
                 what_present ? what : "",
                 what_present ? ": " : "",
                 errnum);
