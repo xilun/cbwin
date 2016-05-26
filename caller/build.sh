f86e29f88a6f53ab3271c6bdd450c878fa375982
Guillaume Knispel <xilun0@gmail.com>
2016-05-26 23:10:21 +0200

    no need to check for wcmd/wstart absence after they have been rm

--- caller/build.sh
+++ caller/build.sh
@@ -3,5 +3,5 @@ set -e
 
 rm -f wrun wcmd wstart
 gcc -O2 -Wall -Wextra -std=c11 -o wrun wrun.c
-if [ ! -x wcmd ] ; then ln wrun wcmd ; fi
-if [ ! -x wstart ] ; then ln wrun wstart ; fi
+ln wrun wcmd
+ln wrun wstart
